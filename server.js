b3b10fa557397c37aee8f3d2bae93171f7ba7af0
Ulisses Malanski <malanskiwork@gamil.com>
2022-11-11 00:14:23 -0300

    add express refactor

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const url = require('url');
+// const http = require('http');
+const express = require('express');
 const freteMetodos = require('./calculadoraFretes');
 
 const port = 3000;
@@ -10,32 +10,54 @@ const {
     infoEndereco
 } = freteMetodos;
 
-const server = http.createServer(async (req, res) => {
+const server = express()
+
+// const server = http.createServer(async (req, res) => {
+server.get('/endereco/cep=:cep', async (req, res) => {
     try {
-        const { 
-            pathname,
-            query: { cep }
-        } = url.parse(req.url, true);
 
-        if (pathname && pathname.includes('/endereco')) {
-            const endereco = await infoEndereco(cep);
-        
-            res.end(JSON.stringify({endereco}));
-        } 
-        
-        if (pathname && pathname.includes('/frete')) {
-            const prazo = await prazoEntrega(cep);
-            const frete = await valorEntrega(cep);
+        const cep = req.params.cep;
 
-            res.end(JSON.stringify({ 'prazo entrega': `${prazo}`, 'valor frete': `${frete}`}));
-        } 
+        const endereco = await infoEndereco(cep);
+
+        res.end(JSON.stringify({endereco}));
+
+        // if (pathname && pathname.includes('/endereco')) {
+        //     res.end(JSON.stringify({endereco}));
+        // } 
+        // if (pathname && pathname.includes('/frete')) {
+        //     const prazo = await prazoEntrega(cep);
+        //     const frete = await valorEntrega(cep);
+        //     res.end(JSON.stringify({ 'prazo entrega': `${prazo}`, 'valor frete': `${frete}`}));
+        // } 
 
     } catch (error) {
-        res
-        .writeHead(error.status ?? 500, { 'Content-Type': 'text/html; charset=utf-8' })
-        .end(JSON.stringify({ message: error.message}));   
-    }
+        // res
+        // .writeHead(error.status ?? 500, { 'Content-Type': 'text/html; charset=utf-8' })
+        // .end(JSON.stringify({ message: error.message}));   
+        res.status(400).json({
+            message: error.message
+        });
+    };
     
+    return;   
+ });
+
+server.get('/frete/cep=:cep', async (req, res) => {
+    try {
+
+        const cep = req.params.cep;
+        
+        const prazo = await prazoEntrega(cep);
+        const frete = await valorEntrega(cep);
+
+        res.end(JSON.stringify({ 'prazo entrega': `${prazo}`, 'valor frete': `${frete}`}));
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    };
     return;   
  });
  
